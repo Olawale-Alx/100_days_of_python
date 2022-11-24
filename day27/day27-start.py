a92a61b351fd260cca71678741c11a49018dc67b
Olawale-Alx <olaleyeolawale2020@outlook.com>
2022-11-24 05:48:34 +0100

    day 27 project - mile to km converter with tkinter GUI

--- day27/day27-start.py
+++ day27/day27-start.py
@@ -7,20 +7,33 @@ window.title('GUI App')
 window.minsize(width=600, height=600)
 
 # Label
-myLabel = tkinter.Label(text='\n\nWelcome, Olawale Olaleye', font=('Mono Space', 28, 'bold'),
+myLabel = tkinter.Label(text='\n\nWelcome, Olawale Olaleye', font=('Mono Space', 28, ['italic', 'bold']),
                         fg='green', bg='yellow')
-myLabel.pack()
+myLabel.grid(column=0, row=0)
 
 
 # Button
 def onclick():
     print('Button got clicked.')
-    myLabel.config(text='Button got clicked')
+    myLabel.config(text=entry.get())
 
 
 my_button = tkinter.Button(text='Click Me!', fg='blue', bg='white', font=('Sans Serif', 18, 'bold'),
                            command=onclick)
-my_button.pack()
+my_button.grid(column=1, row=1)
 
+# Entry - Something like an input field
+entry = tkinter.Entry(bg='violet', bd=2, font=('Mono Space', 16, 'italic'), width=18)
+entry.get()
+entry.grid(column=3, row=2)
+
+# Big text box
+# text_box = tkinter.Text(height=12, width=50, font=('Sans Serif', 12))
+# text_box.grid(column=2, row=3)
+
+# New Button
+new_button = tkinter.Button(text='New Button', bg='yellow', fg='blue', font=('Serif', 18, 'italic'),
+                            highlightcolor='red')
+new_button.grid(column=2, row=0)
 
 window.mainloop()
---
+++ day27/main.py
@@ -0,0 +1,48 @@
+import tkinter
+
+
+window = tkinter.Tk()
+window.title('Mile to Km Converter')
+window.minsize(height=400, width=600)
+window.config(padx=40, pady=60, bg='white')
+
+# First Widget, entry
+entry = tkinter.Entry(width=14, bg='white', bd=2, justify='center',
+                      font=('Mono Space', 14, 'italic'))
+entry.grid(column=1, row=0)
+# entry.config(-padx=2, -pady=3)
+
+# Second Widget, label
+label_one = tkinter.Label(text='Miles', font=('Mono Space', 16, 'bold'), bg='white')
+label_one.grid(column=2, row=0)
+label_one.config(padx=15)
+
+# Third Widget, label
+label_two = tkinter.Label(text='is equal to', font=('Mono Space', 16, 'bold'), bg='white')
+label_two.grid(column=0, row=1)
+label_two.config(padx=5, pady=18)
+
+# Fourth Widget, label
+label_three = tkinter.Label(text=0, font=('Mono Space', 16, 'bold'), bg='white')
+label_three.grid(column=1, row=1)
+
+# Fifth Widget, label
+label_four = tkinter.Label(text='Km', font=('Mono Space', 16, 'bold'), bg='white')
+label_four.grid(column=2, row=1)
+
+
+# Sixth Widget, Button
+def converter():
+    try:
+        num = int(entry.get())
+        mile_to_km = num * 1.609344
+        label_three.config(text=mile_to_km)
+    except ValueError:
+        label_three.config(text='Value must be int')
+
+
+calculator_button = tkinter.Button(text='Calculate', bg='white', fg='black',
+                                   font=('Mono Space', 13, 'bold'), command=converter)
+calculator_button.grid(column=1, row=3)
+
+window.mainloop()
