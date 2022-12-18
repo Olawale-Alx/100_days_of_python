e14d6cb2ae51866dad0e74e21f2299bcbaa29f26
Olawale-Alx <olaleyeolawale2020@outlook.com>
2022-12-18 07:36:43 +0100

    day 32 Project: Birthday message Automation - refactored the code for more functionality

--- day32/birthday_wisher/main.py
+++ day32/birthday_wisher/main.py
@@ -12,14 +12,11 @@ subject = 'Happy Birthday'
 # HINT 1: Create a tuple from today's month and day using datetime. e.g.
 # today = (today_month, today_day)
 now = dt.datetime.now()
-today_month = now.month
-today_day = now.day
-today = (today_month, today_day)
+today = (now.month, now.day)
 
 # HINT 2: Use pandas to read the birthdays.csv
 data = pandas.read_csv('birthdays.csv')
-birthday_month = data.month
-birthday_day = data.day
+
 # HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formatted like this:
 # birthdays_dict = {
 #     (birthday_month, birthday_day): data_row
@@ -28,24 +25,19 @@ birthdays_dict = {(data['month'], data['day']): data for (index, data) in data.i
 
 # HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
 # if (today_month, today_day) in birthdays_dict:
-if (today_month, today_day) in birthdays_dict:
-    with open('letter_templates/letter_1.txt', mode='r') as letters1:
-        letter1 = letters1.read()
-    with open('letter_templates/letter_2.txt', 'r') as letters2:
-        letter2 = letters2.read()
-    with open('letter_templates/letter_3.txt', 'r') as letters3:
-        letter3 = letters3.read()
-    letters = (letter1, letter2, letter3)
-    random_letter = letters[random.randint(0, 2)]
-    print_letter = random_letter.replace('[NAME]', 'Ola')
-    # print(print_letter)
+if today in birthdays_dict:
+    file_path = f'letter_templates/letter_{random.randint(1, 3)}.txt'
+    birthday_person = birthdays_dict[today]
+    with open(file_path, mode='r') as letters:
+        letter = letters.read()
+        letter.replace('[NAME]', birthday_person['name'])
 
     # 4. Send the letter generated in step 3 to that person's email address.
     # HINT: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
     with smtplib.SMTP_SSL(host='smtp.mail.yahoo.com', port=465) as connection:
         connection.login(user=my_email, password=secret)
         connection.sendmail(from_addr=my_email, to_addrs=recipient,
-                            msg=f'Subject: {subject}\n\n\n{print_letter}')
+                            msg=f'Subject: {subject}\n\n\n{letter}')
 
 else:
     print(False)
