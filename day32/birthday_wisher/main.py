aac13129a03f6639bff44393bed56fac3529a560
Olawale-Alx <olaleyeolawale2020@outlook.com>
2022-12-18 07:28:48 +0100

    day 32 Project: Birthday message Automation

--- day32/birthday_wisher/main.py
+++ day32/birthday_wisher/main.py
@@ -1,43 +1,52 @@
-##################### Normal Starting Project ######################
+import smtplib
+import datetime as dt
+import random
+import pandas
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. e.g.
-#name,email,year,month,day
-#YourName,your_own@email.com,today_year,today_month,today_day
+my_email = 'iolaleye62@yahoo.com'
+recipient = 'olaleyeolawale2020@outlook.com'
+secret = 'vbovtfuubybpctnr'
+subject = 'Happy Birthday'
 
 # 2. Check if today matches a birthday in the birthdays.csv
 # HINT 1: Create a tuple from today's month and day using datetime. e.g.
 # today = (today_month, today_day)
+now = dt.datetime.now()
+today_month = now.month
+today_day = now.day
+today = (today_month, today_day)
 
 # HINT 2: Use pandas to read the birthdays.csv
-
-# HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
+data = pandas.read_csv('birthdays.csv')
+birthday_month = data.month
+birthday_day = data.day
+# HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formatted like this:
 # birthdays_dict = {
 #     (birthday_month, birthday_day): data_row
 # }
-#Dictionary comprehension template for pandas DataFrame looks like this:
-# new_dict = {new_key: new_value for (index, data_row) in data.iterrows()}
-#e.g. if the birthdays.csv looked like this:
-# name,email,year,month,day
-# Angela,angela@email.com,1995,12,24
-#Then the birthdays_dict should look like this:
-# birthdays_dict = {
-#     (12, 24): Angela,angela@email.com,1995,12,24
-# }
+birthdays_dict = {(data['month'], data['day']): data for (index, data) in data.iterrows()}
 
-#HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
+# HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
 # if (today_month, today_day) in birthdays_dict:
+if (today_month, today_day) in birthdays_dict:
+    with open('letter_templates/letter_1.txt', mode='r') as letters1:
+        letter1 = letters1.read()
+    with open('letter_templates/letter_2.txt', 'r') as letters2:
+        letter2 = letters2.read()
+    with open('letter_templates/letter_3.txt', 'r') as letters3:
+        letter3 = letters3.read()
+    letters = (letter1, letter2, letter3)
+    random_letter = letters[random.randint(0, 2)]
+    print_letter = random_letter.replace('[NAME]', 'Ola')
+    # print(print_letter)
 
-# 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT 1: Think about the relative file path to open each letter. 
-# HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
-# HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
+    # 4. Send the letter generated in step 3 to that person's email address.
+    # HINT: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
+    with smtplib.SMTP_SSL(host='smtp.mail.yahoo.com', port=465) as connection:
+        connection.login(user=my_email, password=secret)
+        connection.sendmail(from_addr=my_email, to_addrs=recipient,
+                            msg=f'Subject: {subject}\n\n\n{print_letter}')
 
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-# HINT 2: Remember to call .starttls()
-# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
-# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
-
-
+else:
+    print(False)
 
