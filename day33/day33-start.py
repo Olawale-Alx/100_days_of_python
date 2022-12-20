e2685e9f35246ff7215b5eefe06f26789af1a271
Olawale-Alx <olaleyeolawale2020@outlook.com>
2022-12-21 00:50:35 +0100

    day33 Project: ISS location monitoring

--- day33/day33-start.py
+++ day33/day33-start.py
@@ -9,3 +9,17 @@ longitude = data['iss_position']['longitude']
 
 iss_current_position = (latitude, longitude)
 print(iss_current_position)
+
+# parameters = {
+#     'lat': 6.740147,
+#     'lng': 3.422076,
+#     'date': '2022-12-15',
+#     'formatted': 0
+# }
+#
+# api_request = requests.get(url='https://api.sunrise-sunset.org/json', params=parameters)
+# api_request.raise_for_status()
+# data = api_request.json()
+# sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
+# sunset = data['results']['sunset'].split('T')[1].split(':')[0]
+# print(f'Sunrise: {sunrise} Sunset: {sunset}')
---
+++ day33/issoverhead/main.py
@@ -0,0 +1,59 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_LAT = 6.740147  # Your latitude
+MY_LONG = 3.422076   # Your longitude
+my_email = 'iolaleye62@yahoo.com'
+recipient = 'olaleyeolawale2020@outlook.com'
+secret = 'vbovtfuubybpctnr'
+
+
+def check_position():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and \
+            MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+    else:
+        return False
+
+
+def check_time():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    dark = time_now.hour
+
+    if dark >= sunset or dark <= sunrise:
+        return True
+
+
+while True:
+    # BONUS: run the code every 60 seconds
+    time.sleep(60)
+    # If the ISS is close to my current position, and it is currently dark
+    if check_position() and check_time():
+        # Then email me to tell me to look up.
+        with smtplib.SMTP_SSL(host='smtp.mail.yahoo.com', port=465) as connection:
+            connection.login(user=my_email, password=secret)
+            connection.sendmail(from_addr=my_email, to_addrs=recipient,
+                                msg='Subject: Look Up!!!\n\n\nOlawale,\n\nLook up!'
+                                    'The ISS is in your location.')
