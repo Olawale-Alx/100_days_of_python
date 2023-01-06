e4caaf500198e291bcb5f0f5e84f217d5849915f
Olawale-Alx <olaleyeolawale2020@outlook.com>
2023-01-06 17:05:53 +0100

    day 32 - Removed SMPT credentials

--- day32/day32-start.py
+++ day32/day32-start.py
@@ -1,16 +1,16 @@
 import smtplib
 import datetime as dt
+import os
 
-# MY_EMAIL = 'iolaleye62@yahoo.com'
-# # SERVER = 'smtp.gmail.com'
-# my_password = 'vbovtfuubybpctnr'
-# # message = 'Hello, World'
-#
-# with smtplib.SMTP_SSL(host='smtp.mail.yahoo.com', port=465) as connection:
-#     # connection.starttls()
-#     connection.login(user=MY_EMAIL, password=my_password)
-#     connection.sendmail(from_addr=MY_EMAIL, to_addrs='olaleyeolawale2020@outlook.com',
-#                         msg=f'Subject: Introduction\n\n\nHello, World. My email address is {MY_EMAIL}')
+MY_EMAIL = os.environ.get('MY_EMAIL')
+my_password = os.environ.get('SECRET')
+# message = 'Hello, World'
+
+with smtplib.SMTP_SSL(host='', port=0) as connection:
+    # connection.starttls()
+    connection.login(user=MY_EMAIL, password=my_password)
+    connection.sendmail(from_addr=MY_EMAIL, to_addrs='olaleyeolawale2020@outlook.com',
+                        msg=f'Subject: Introduction\n\n\nHello, World. My email address is {MY_EMAIL}')
 
 days = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday',
         6: 'Sunday'}
